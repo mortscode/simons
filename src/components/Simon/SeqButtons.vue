ab7091b4f923c05247a2e24aea6015d22785d05b
Scot Mortimer <scot.mortimer@gmail.com>
2017-07-24 15:38:55 -0700

    button animations

--- src/components/Simon/SeqButtons.vue
+++ src/components/Simon/SeqButtons.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="button__wrapper">
     <div v-for="btn in this.seqButtons" class="button" v-bind:key="btn.id">
-      <div class="button__inner" :data-color="btn.color"></div>
+      <button class="button__inner"
+        :class="[{active: isActive === btn.color}, btn.color]"
+        :data-clicked="btn.clicked"
+        :data-color="btn.color"
+        @click="activate(btn.color)">
+      </button>
     </div>
   </div>
 </template>
@@ -11,12 +16,17 @@
 
   export default {
     methods: {
-      displayDetails(id) {
-        this.$router.push({ name: 'detail', params: { id } });
+      activate(btn) {
+        this.isActive = btn;
+
+        setTimeout(() => {
+          this.isActive = '';
+        }, 300);
       },
     },
     data() {
       return {
+        isActive: '',
         seqButtons: data.seqButtons,
       };
     },
@@ -40,25 +50,61 @@
     }
 
     &__inner {
+      position: relative;
+      outline: 0;
+      border: 0;
       border-radius: 50%;
       flex: 0 0 90%;
       height: 90%;
-    }
 
-    [data-color=blue] {
-      background-color: $blue;
-    }
+      &::before {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        transform: scale(0.5) translate(-50%, -50%);
+        transform-origin: 0 0;
+        transition: transform 150ms ease-out, border 150ms ease-out;
+      }
 
-    [data-color=green] {
-      background-color: $green;
-    }
+      &.blue {
+        background-color: $blue;
 
-    [data-color=red] {
-      background-color: $red;
-    }
+        &::before {
+          border: 0px solid $blue;
+        }
+      }
 
-    [data-color=yellow] {
-      background-color: $yellow;
+      &.green {
+        background-color: $green;
+
+        &::before {
+          border: 0px solid $green;
+        }
+      }
+
+      &.red {
+        background-color: $red;
+
+        &::before {
+          border: 0px solid $red;
+        }
+      }
+
+      &.yellow {
+        background-color: $yellow;
+
+        &::before {
+          border: 0px solid $yellow;
+        }
+      }
+
+      &.active::before {
+        animation: activeButton 300ms;
+      }
     }
   }
 </style>
--- src/data.js
+++ src/data.js
@@ -2,21 +2,25 @@ export default {
   seqButtons: [
     {
       id: 1,
+      active: false,
       name: 'button-1',
       color: 'blue',
     },
     {
       id: 2,
+      active: false,
       name: 'button-2',
       color: 'yellow',
     },
     {
       id: 3,
+      active: false,
       name: 'button-3',
       color: 'red',
     },
     {
       id: 4,
+      active: false,
       name: 'button-4',
       color: 'green',
     },
